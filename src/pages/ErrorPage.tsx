72168c212832d9ba61c14dbb3b4a2be465f08417
rohit kumar <rohitkumar38344@gmail.com>
2026-04-02 11:03:14 +0530

    style(error page): upated error page info

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,4 +1,7 @@
+import { useRouteError } from 'react-router-dom';
+
 export default function ErrorPage() {
+  const error = useRouteError();
   return (
     <>
       {/*
@@ -16,7 +19,7 @@ export default function ErrorPage() {
             Page not found
           </h1>
           <p className="mt-6 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
-            Sorry, we couldn’t find the page you’re looking for.
+            <i>{error.statusText || error.message}</i>
           </p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <a
